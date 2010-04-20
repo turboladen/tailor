8b72d1cefc0336d201eb2e3791b3e38634a3f274
Steve Loveless <steve.loveless@gmail.com>
2010-04-20 00:01:49 -0700

    Added ability for checking camel case on class names.  Made camel_case? in to 2 methods (1 for class and 1 for method).  Moved some cucumber step defs to world.rb since they'll be used in a number of places. Added files for testing camel case vs. snake case.

---
+++ features/step_definitions/case_checking_steps.rb
@@ -0,0 +1,36 @@
+require 'ruby_style_checker/file_line'
+
+def contains_camel_case? keyword
+  @ruby_source = File.open(@file_list[0], 'r')
+
+  contains_camel_case = false
+  @ruby_source.each_line do |source_line|
+    line = FileLine.new source_line
+
+    if keyword.eql? "method"
+      line.camel_case_method? ? (return true) : (return false)
+    elsif keyword.eql? "class"
+      line.camel_case_class? ? (return true) : (return false)
+    end
+  end
+end
+
+Given /^the file contains a "([^\"]*)" that has a camel\-cased name$/ do |keyword|
+  contains_camel_case?(keyword).should be_true
+end
+
+Given /^the file contains a "([^\"]*)" that has a snake\-cased name$/ do |keyword|
+    contains_camel_case?(keyword).should be_false
+end
+
+Then /^the checker should tell me I have a camel\-cased method name$/ do
+  @result.should include("Method name uses camel case")
+end
+
+Then /^the checker shouldn't tell me the class name is camel\-case$/ do
+  @result.should_not include("Class name uses camel case")
+end
+
+Then /^the checker should tell me the class name is not camel\-case$/ do
+  @result.should include("Class name does NOT use camel case")
+end
--- features/support/world.rb
+++ features/support/world.rb
@@ -4,6 +4,26 @@ require 'ruby_style_checker'
 
 include RubyStyleChecker
 
+
+# Counts keywords in the file provided.
+# 
+# @param [String] file Path to the file to check
+# @param [String] keyword Keyword to count
+# @return [Number] Number of keywords counted
+def count_keywords file, keyword
+  ruby_source = File.open(file, 'r')
+  
+  count = 0
+  ruby_source.each_line do |line|
+    if line =~ /^#{keyword}/
+      count =+ 1
+    end
+  end
+  ruby_source.close
+  count
+end
+
+
 # Prep for the testing
 Before do
   @ruby_style_checker = "#{File.dirname(__FILE__)}/../../bin/ruby_style_checker"
@@ -18,6 +38,12 @@ Given /^I have a project directory "([^\"]*)"$/ do |project_dir|
   File.exists?(project_dir).should be_true
   File.directory?(project_dir).should be_true
   @project_dir = project_dir
+end
+
+Given /^the file contains only "([^\"]*)" "([^\"]*)" statement$/ do |count_in_spec, keyword|
+  #count_in_file = count_keywords(@ruby_source, keyword)
+  count_in_file = count_keywords(@file_list[0], keyword)
+  count_in_file.should == count_in_spec.to_i
 end
 
 Given /^I have "([^\"]*)" file in my project$/ do |file_count|
