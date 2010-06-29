7d1b4717fed97e1d44a13126ef823c95a1ad6c7e
Steve Loveless <steve.loveless@gmail.com>
2010-06-28 21:36:12 -0700

    Added lines for test cases

--- features/support/1_file_with_bad_comma_spacing/bad_comma_spacing.rb
+++ features/support/1_file_with_bad_comma_spacing/bad_comma_spacing.rb
@@ -1,5 +1,43 @@
-# This is a sample which ,when checked,should show bad-comma errors. Comma errors,  
-# and stuff,   make source difficult to read!
-def do_something this ,that,  other
-  puts "We ,I mean I,am doing,  er, something!"
+# Perfect spacing after a comma, in comment and method
+def do_something this, that, other
+  puts "We, I mean I, am doing, er, something!"
 end
+
+# More than one space,  after a comma,  in a comment & method
+def do_something this,  that,   other
+  puts "We,  I mean I,  am doing,  er, something!"
+end
+
+# No space,after a comma,in a comment & method
+def do_something this,that,other
+  puts "We,I mean I ,am doing,er   ,something!"
+end
+
+# Spaces , before a comma ,in a comment & method
+def do_something this , that ,other
+  puts "We , I mean I ,am doing ,er   , something!"
+end
+
+# Perfect spacing around a comma, in an array
+thing = [ 1,  2,  3 ]
+
+# More than one space, after elements in an array
+thing = [ 1,  2,  3 ]
+
+# No space, after elements in an array
+thing = [ 1,2,3 ]
+
+# Spaces, before elements in an array
+thing = [ 1 , 2  ,3 ]
+
+# Perfect spacing around a comma, in a hash
+thing = { :one => 1, :two => 2, :three => 3 }
+
+# More than one space, after elements in a hash
+thing = { :one => 1,  :two => 2,   :three => 3 }
+
+# No space, after elements in an array
+thing = { :one => 1,:two => 2,:three => 3 }
+
+# Spaces, before elements in an array
+thing = { :one => 1 , :two => 2  , :three => 3 }
