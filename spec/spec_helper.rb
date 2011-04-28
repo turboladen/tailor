a8479e39d248c7d1af910553b4f996348d997b41
Steve Loveless <steve.loveless@gmail.com>
2011-04-28 10:42:36 -0700

    Converted to use Rspec 2.x

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
-begin
-  require 'spec'
-rescue LoadError
-  require 'rubygems' unless ENV['NO_RUBYGEMS']
-  gem 'rspec'
-  require 'spec'
-end
-
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 require 'tailor'
 
