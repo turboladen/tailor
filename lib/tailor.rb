877ad097b3405cfd35ca35e18ae1bb7cd6103355
Steve Loveless <steve.loveless@gmail.com>
2012-02-01 14:52:30 -0800

    Moved base_methods back to tailor.rb to simplify.  Added first test for #check_style.

--- lib/tailor.rb
+++ lib/tailor.rb
@@ -1,8 +1,56 @@
 require 'log_switch'
-require_relative 'tailor/base_methods'
 
 
 class Tailor
   extend LogSwitch
-  include Tailor::BaseMethods
+
+  class << self
+    # Main entry-point method.
+    #
+    # @param [String] path File or directory to check files in.
+    def check_style(path)
+      if File.file?(path)
+        Tailor.log "Checking style of a single file."
+        check_file(path)
+      elsif File.directory?(path)
+        Tailor.log "Checking style of a directory."
+        Dir.glob(path).each { |f| check_file(f) }
+      else
+        raise "Not sure what this is: #{path}..."
+      end
+    end
+
+    # @return [Hash] List of problem types and how many.
+    def check_file file
+      file_text = File.open(file, 'r').read
+      lexer = Tailor::LineLexer.new(file_text)
+      lexer.lex
+
+      lexer.problems
+    end
+
+    # @todo This could delegate to Ruport (or something similar) for allowing
+    #   output of different types.
+    def print_report
+      puts "#{problem_count} errors."
+    end
+
+    # @return [Hash]
+    def problems
+      @problems ||= {}
+    end
+
+    # @return [Fixnum] The number of problems found so far.
+    def problem_count
+      problems.empty? ? 0 : problems.values.inject(:+)
+    end
+
+    # Checks to see if +path_to_check+ is a real file or directory.
+    #
+    # @param [String] path_to_check
+    # @return [Boolean]
+    def checkable? path_to_check
+      File.file?(path_to_check) || File.directory?(path_to_check)
+    end
+  end
 end
--- spec/tailor_spec.rb
+++ spec/tailor_spec.rb
@@ -2,4 +2,20 @@ require_relative 'spec_helper'
 require 'tailor'
 
 describe Tailor do
+  describe "#problems" do
+    specify { Tailor.problems.should be_a Hash }
+    specify { Tailor.problems.should be_empty }
+  end
+
+  describe "#check_style" do
+    context "single file" do
+      before { File.stub(:file?).and_return true }
+      after { File.unstub(:file?) }
+
+      it "calls #check_file" do
+        Tailor.should_receive(:check_file).once
+        Tailor.check_style("a_file.rb")
+      end
+    end
+  end
 end
