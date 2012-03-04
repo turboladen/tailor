e2425ccff8960b478a2f7fead5c2f50a51b28f71
Steve Loveless <steve.loveless@gmail.com>
2012-03-03 17:24:47 -0800

    Added tests for #modifier_keyword? and did some clean up

--- spec/tailor/line_lexer_spec.rb
+++ spec/tailor/line_lexer_spec.rb
@@ -1,20 +1,37 @@
 require_relative '../spec_helper'
+require 'tailor/line_lexer'
 
 describe Tailor::LineLexer do
-  describe "#initialize" do
-    subject { Tailor::LineLexer.new("") }
+  let(:file_text) do
+    ""
+  end
 
-    it "sets @indentation_tracker to an empty Array" do
-      subject.instance_variable_get(:@indentation_tracker).should be_an Array
-      subject.instance_variable_get(:@indentation_tracker).should be_empty
+  before do
+    File.stub_chain(:open, :read).and_return file_text
+    Tailor.stub :log
+    Tailor.stub_chain(:config, :[]).and_return({ spaces: 2,
+      allow_hard_tabs: false })
+  end
+
+  after do
+    Tailor.unstub :log
+    File.unstub :open
+  end
+
+  subject { Tailor::LineLexer.new(file_text) }
+
+  describe "#initialize" do
+    it "opens and reads the file by the name passed in" do
+      file_name = "test"
+      File.should_receive(:open).with("test", 'r')
+      Tailor::LineLexer.new(file_name)
     end
 
-    it "sets @proper_indentation a Hash with :this_line and :next_line" do
-      subject.instance_variable_get(:@proper_indentation).should be_a Hash
-      subject.instance_variable_get(:@proper_indentation).keys.
-        should == [
-        :this_line,
-        :next_line]
+    it "sets @proper_indentation to an Hash with :this_line and :next_line keys = 0" do
+      proper_indentation = subject.instance_variable_get(:@proper_indentation)
+      proper_indentation.should be_a Hash
+      proper_indentation[:this_line].should be_zero
+      proper_indentation[:next_line].should be_zero
     end
   end
 
@@ -36,11 +53,9 @@ describe Tailor::LineLexer do
       ]
     end
 
-    subject { Tailor::LineLexer.new("") }
-
     it "returns all lexed output from line 1 when self.lineno is 1" do
-      subject.stub(:lineno).and_return 1
-      subject.current_lex(lexed_output).should ==  [[[1, 0], :on_ident, "require"],
+      subject.stub(:lineno).and_return(1)
+      subject.current_lex(lexed_output).should == [[[1, 0], :on_ident, "require"],
         [[1, 7], :on_sp, " "],
         [[1, 8], :on_tstring_beg, "'"],
         [[1, 9], :on_tstring_content, "log_switch"],
@@ -50,7 +65,43 @@ describe Tailor::LineLexer do
     end
   end
 
-  describe "#on_nl" do
+  describe "#modifier_keyword?" do
+    context "the current line has a keyword that is also a modifier" do
+      context "the keyword is acting as a modifier" do
+        let(:file_text) do
+          <<-FILE
+puts "hi" if true == true
+          FILE
+        end
 
+        before do
+          File.stub_chain(:open, :read).and_return file_text
+        end
+
+        it "returns true" do
+          subject.stub(:lineno).and_return 1
+          subject.instance_variable_set(:@file_text, file_text)
+          subject.modifier_keyword?("if").should be_true
+        end
+      end
+
+      context "they keyword is NOT acting as a modifier" do
+        let(:file_text) do
+          <<-FILE
+if true == true; puts "hi"; end
+          FILE
+        end
+
+        before do
+          File.stub_chain(:open, :read).and_return file_text
+        end
+
+        it "returns false" do
+          subject.stub(:lineno).and_return 1
+          subject.instance_variable_set(:@file_text, file_text)
+          subject.modifier_keyword?("if").should be_false
+        end
+      end
+    end
   end
 end
