bcf7f6fc562325f5176b3ceb6542f0536fb9003d
Steve Loveless <steve.loveless@gmail.com>
2012-03-12 21:17:13 -0700

    Moved methods dealing with vertical whitespace into a separate module

--- spec/tailor/ruler_spec.rb
+++ spec/tailor/ruler_spec.rb
@@ -44,62 +44,6 @@ describe Tailor::Ruler do
     end
   end
 
-  describe "#count_trailing_newlines" do
-    context "text contains 0 trailing \\n" do
-      let(:text) { "text" }
-      specify { subject.count_trailing_newlines(text).should be_zero }
-    end
-
-    context "text contains 1 trailing \\n" do
-      let(:text) { "text\n" }
-      specify { subject.count_trailing_newlines(text).should == 1 }
-    end
-  end
-
-  describe "#ensure_trailing_newline" do
-    context "text contains a trailing newline already" do
-      let!(:text) { "text\n" }
-
-      it "doesn't alter the text" do
-        subject.ensure_trailing_newline(text).should == text
-      end
-    end
-
-    context "text does not contain a trailing newline" do
-      let!(:text) { "text" }
-
-      it "adds a newline at the end" do
-        subject.ensure_trailing_newline(text).should == text + "\n"
-      end
-
-      context "@config[:trailing_newlines] > 0" do
-        it "logs a problem" do
-          subject.instance_variable_set(:@problems, [])
-          subject.ensure_trailing_newline(text)
-
-          problems = subject.instance_variable_get(:@problems)
-          problems.size.should == 1
-          problems.first[:type].should == :trailing_newlines
-        end
-      end
-
-      context "@config[:trailing_newlines] == 0" do
-        before do
-          Tailor.stub(:config).and_return({
-            vertical_whitespace: { trailing_newlines: 0 }
-          })
-        end
-
-        it "doesn't log a problem" do
-          subject.instance_variable_set(:@problems, [])
-          subject.ensure_trailing_newline(text)
-
-          problems = subject.instance_variable_get(:@problems)
-          problems.size.should be_zero
-        end
-      end
-    end
-  end
 
   describe "#current_lex" do
     let(:lexed_output) do
