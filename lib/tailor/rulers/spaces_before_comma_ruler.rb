f0bbda4540a5877fb97474feeb898bc6c2195742
Steve Loveless <steve.loveless@gmail.com>
2012-03-25 17:24:46 -0700

    Fixed long line and extra spaces

--- lib/tailor/rulers/spaces_before_comma_ruler.rb
+++ lib/tailor/rulers/spaces_before_comma_ruler.rb
@@ -7,7 +7,7 @@ class Tailor
         super(config)
         @comma_columns = []
       end
-      
+
       def comma_update(text_line, lineno, column)
         @comma_columns << column
       end
@@ -17,23 +17,27 @@ class Tailor
           column_event = lexed_line.event_at(c)
           event_index = lexed_line.index(column_event)
           next if event_index.nil?
-          
+
           previous_event = lexed_line.at(event_index - 1)
-          actual_spaces = previous_event[1] != :on_sp ? 0 : previous_event.last.size
-          
+          actual_spaces = if previous_event[1] != :on_sp
+            0
+          else
+            previous_event.last.size
+          end
+
           if actual_spaces != @config
             @problems << Problem.new(:spaces_before_comma, lineno, c - 1,
               { actual_spaces: actual_spaces, should_have: @config })
           end
         end
-        
+
         @comma_columns.clear
       end
 
       def ignored_nl_update(lexed_line, lineno, column)
         check_spaces_before_comma(lexed_line, lineno)
       end
-      
+
       def nl_update(lexed_line, lineno, column)
         ignored_nl_update(lexed_line, lineno, column)
       end
