162866141fdcb4aaaeef66e55d0e547e4b8fc6bb
Steve Loveless <steve.loveless@gmail.com>
2012-03-30 20:30:42 -0700

    Added tests and docs.

--- lib/tailor/reporter.rb
+++ lib/tailor/reporter.rb
@@ -1,7 +1,15 @@
 class Tailor
+  
+  # Objects of this type are responsible for sending the right data to report
+  # formatters.
   class Reporter
     attr_reader :formatters
 
+    # For each in +formats+, it creates a new
+    # +Tailor::Formatter::#{formatter.capitalize}+ object and adds it to
+    # +@formatters+.
+    #
+    # @param [Array] formats A list of formatters to use for generating reports.
     def initialize(*formats)
       @formatters = []
 
@@ -11,12 +19,26 @@ class Tailor
       end
     end
     
+    # Sends the data to each +@formatters+ to generate the report of problems
+    # for the file that was just critiqued.  A problem is in the format:
+    #
+    #   { 'path/to/file.rb' => [Problem1, Problem2, etc.]}
+    #
+    # ...where Problem1 and Problem2 are of type {Tailor::Problem}.
+    #
+    # @param [Hash] file_problems
+    # @param [Symbol,String] label The label of the file_set that defines the
+    #   problems in +file_problems+.
     def file_report(file_problems, label)
       @formatters.each do |formatter|
         formatter.file_report(file_problems, label)
       end
     end
     
+    # Sends the data to each +@formatters+ to generate the reports of problems
+    # for all files that were just critiqued.
+    #
+    # @param [Hash] all_problems 
     def summary_report(all_problems)
       @formatters.each do |formatter|
         formatter.summary_report(all_problems)
--- spec/tailor/reporter_spec.rb
+++ spec/tailor/reporter_spec.rb
@@ -6,10 +6,48 @@ describe Tailor::Reporter do
     context "text formatter" do
       let(:formats) { ['text'] }
 
-      it "creates a new Text object" do
+      it "creates a new Formatter object of the type passed in" do
         reporter = Tailor::Reporter.new(formats)
         reporter.formatters.first.should be_a Tailor::Formatter::Text
       end
+    end
+  end
+  
+  describe "#file_report" do
+    let(:file_problems) { double "file problems" }
+    let(:formatter) { double "Tailor::Formatter::SomeFormatter" }
+    
+    subject do
+      t = Tailor::Reporter.new
+      t.instance_variable_set(:@formatters, [formatter])
+      
+      t
+    end 
+    
+    it "calls #file_report on each @formatters" do
+      label = :some_label
+      formatter.should_receive(:file_report).with(file_problems, label)
+
+      subject.file_report(file_problems, label)
+    end
+  end
+
+  describe "#summary_report" do
+    let(:all_problems) { double "all problems" }
+    let(:formatter) { double "Tailor::Formatter::SomeFormatter" }
+
+    subject do
+      t = Tailor::Reporter.new
+      t.instance_variable_set(:@formatters, [formatter])
+
+      t
+    end
+
+    it "calls #file_report on each @formatters" do
+      label = :some_label
+      formatter.should_receive(:summary_report).with(all_problems)
+
+      subject.summary_report(all_problems)
     end
   end
 end
