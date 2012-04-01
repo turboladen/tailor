11917c10d10a9fda0549a35342abdec20fee5e20
Steve Loveless <steve.loveless@gmail.com>
2012-04-01 15:32:08 -0700

    Refactored ruler and fixed tests
    #count_spaces shouldn't return nil if there's 0 spaces before.  Changed
    it to set @do_validation to false when there's a case when the
    validation shouldn't be ran.  Also commented out an if statement, as I
    don't understand why it's in there but the cuke tests are broken right
    now to determine if I'll break things by permanently removing it.

--- lib/tailor/rulers/spaces_before_lbrace_ruler.rb
+++ lib/tailor/rulers/spaces_before_lbrace_ruler.rb
@@ -2,7 +2,7 @@ require_relative '../ruler'
 
 class Tailor
   module Rulers
-    
+
     # Detects spaces before a '{' as given by +@config+.  It skips checking
     # when:
     # * it's the first char in the line.
@@ -11,7 +11,13 @@ class Tailor
     # * the char before it is a '['.
     # * it's only preceded by spaces.
     class SpacesBeforeLbraceRuler < Tailor::Ruler
-      
+      def initialize(config)
+        super(config)
+        @do_validation = true
+      end
+
+      # Counts the spaces before the '{'.
+      #
       # @param [LexedLine] lexed_line
       # @param [Fixnum] column
       # @return [Fixnum] The number of spaces before the lbrace.
@@ -21,29 +27,56 @@ class Tailor
         previous_event = lexed_line.at(current_index - 1)
         log "Previous event: #{previous_event}"
 
-        if column.zero? || previous_event.nil? ||
-          previous_event[1] == :on_embexpr_beg ||
-          previous_event[1] == :on_lparen ||
-          previous_event[1] == :on_lbracket
-          return nil
+        if column.zero? || previous_event.nil?
+          log "lbrace must be at the beginning of the line."
+          @do_validation = false
+          return 0
         end
-        
+
+        if previous_event[1] == :on_embexpr_beg
+          log "lbrace comes after a '\#{'."
+          @do_validation = false
+          return 0
+        end
+
+        if previous_event[1] == :on_lparen
+          log "lbrace comes after a '('."
+          @do_validation = false
+          return 0
+        end
+
+        if previous_event[1] == :on_lbracket
+          log "lbrace comes after a '['."
+          @do_validation = false
+          return 0
+        end
+
         return 0 if previous_event[1] != :on_sp
-        return nil if current_index - 2 < 0
-        
+
+        # todo: I forget why this is here...
+        #if current_index - 2 < 0
+        #  log "current_index - 2 < 0.  Why is this statement here?"
+        #  @do_validation = false
+        #  return 0
+        #end
+
         previous_event.last.size
       end
 
+      # Called by {Lexer} when :on_lbrace is encountered.
+      #
+      # @param [LexedLine] lexed_line
+      # @param [Fixnum] lineno
+      # @param [Fixnum] column
       def lbrace_update(lexed_line, lineno, column)
         count = count_spaces(lexed_line, column)
-        
-        if count.nil?
-          log "lbrace must be at the beginning of the line."
+
+        if @do_validation == false
           return
         else
           log "Found #{count} space(s) before lbrace."
         end
-        
+
         if count != @config
           @problems << Problem.new(:spaces_before_lbrace, lineno, column,
             { actual_spaces: count, should_have: @config })
--- spec/tailor/rulers/spaces_before_lbrace_ruler_spec.rb
+++ spec/tailor/rulers/spaces_before_lbrace_ruler_spec.rb
@@ -2,19 +2,28 @@ require_relative '../../spec_helper'
 require 'tailor/rulers/spaces_before_lbrace_ruler'
 
 describe Tailor::Rulers::SpacesBeforeLbraceRuler do
+  subject { Tailor::Rulers::SpacesBeforeLbraceRuler.new 1 }
+  #before { Tailor::Logger.stub(:log) }
+
   describe "#count_spaces" do
-    context "lbrace is the first char in the line" do
+    context "lexed_line.event_index is 0" do
       let(:lexed_line) do
         l = double "LexedLine"
         l.stub(:event_index).and_return 0
         l.stub(:at).and_return nil
-        
+
         l
       end
-      
-      specify { subject.count_spaces(lexed_line, 1).should be_nil }
+
+      specify { subject.count_spaces(lexed_line, 1).should be_zero }
+
+      it "sets @do_validation to false" do
+        expect { subject.count_spaces(lexed_line, 1) }.to
+          change{subject.instance_variable_get(:@do_validation)}.from(true).
+          to(false)
+      end
     end
-    
+
     context "no space before lbrace" do
       let(:lexed_line) do
         l = double "LexedLine"
@@ -26,7 +35,7 @@ describe Tailor::Rulers::SpacesBeforeLbraceRuler do
 
       specify { subject.count_spaces(lexed_line, 1).should be_zero }
     end
-    
+
     context "1 space before lbrace" do
       let(:lexed_line) do
         l = double "LexedLine"
@@ -38,17 +47,17 @@ describe Tailor::Rulers::SpacesBeforeLbraceRuler do
 
       specify { subject.count_spaces(lexed_line, 1).should == 1 }
     end
-    
+
     context "> 1 space before lbrace" do
       let(:lexed_line) do
         l = double "LexedLine"
         l.stub(:event_index).and_return 1
-        l.stub(:at).and_return [[10, 0], :on_sp, "  "]
+        l.stub(:at).and_return [[10, 1], :on_sp, "  "]
 
         l
       end
 
-      specify { subject.count_spaces(lexed_line, 1).should == 2 }
+      specify { subject.count_spaces(lexed_line, 3).should == 2 }
     end
   end
 end
