2b945ecaf974354179ea06af8cb7b3be961bce80
Steve Loveless <steve.loveless@gmail.com>
2012-04-03 17:54:41 -0700

    Shortened the logger message format

--- lib/tailor/logger.rb
+++ lib/tailor/logger.rb
@@ -3,5 +3,17 @@ require 'log_switch'
 class Tailor
   class Logger
     extend LogSwitch
+    
+    def self.logger
+      return @logger if @logger
+      
+      @logger ||= ::Logger.new $stdout
+      
+      def @logger.format_message(level, time, progname, msg)
+        "[#{time.strftime("%Y-%m-%d %H:%M:%S")}]  #{msg}\n"
+      end
+
+      @logger
+    end
   end
 end
