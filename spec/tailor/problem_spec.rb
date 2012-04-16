70f1109b8f6a9e92a1843e5818f15895bcc353de
Steve Loveless <steve.loveless@gmail.com>
2012-04-15 22:30:11 -0700

    Fixed tests

--- spec/tailor/problem_spec.rb
+++ spec/tailor/problem_spec.rb
@@ -15,60 +15,28 @@ describe Tailor::Problem do
     end
 
     it "sets self[:type] to the type param" do
-      Tailor::Problem.new(:test, lineno, column).should include(type: :test)
+      Tailor::Problem.new(:test, lineno, column, "", :b).
+        should include(type: :test)
     end
 
     it "sets self[:line] to the lineno param" do
-      Tailor::Problem.new(:test, lineno, column).should include(line: lineno)
+      Tailor::Problem.new(:test, lineno, column, "", :c).
+        should include(line: lineno)
     end
 
-    it "sets self[:column] to 'column' from the binding" do
-      Tailor::Problem.new(:test, lineno, column).should include(column: column)
+    it "sets self[:column] to the column param" do
+      Tailor::Problem.new(:test, lineno, column, "", :d).
+        should include(column: column)
     end
 
-    it "sets self[:message] to what's returned from #message for @type" do
-      Tailor::Problem.any_instance.should_receive(:message).with(:test).
-        and_return("test message")
-
-      problem = Tailor::Problem.new(:test, lineno, column)
-      problem.should include(message: "test message")
-    end
-  end
-
-  describe "#message" do
-    before do
-      Tailor::Problem.any_instance.stub(:set_values)
+    it "sets self[:message] to the message param" do
+      Tailor::Problem.new(:test, lineno, column, "test", :d).
+        should include(message: "test")
     end
 
-    context "type is :indentation" do
-      it "builds a successful message" do
-        options = { actual_indentation: 10, should_be_at: 97 }
-        problem = Tailor::Problem.new(:test, lineno, column, options)
-        problem.message(:indentation).should match /10.*97/
-      end
-    end
-
-    context "type is :trailing_newlines" do
-      it "builds a successful message" do
-        options = { actual_trailing_newlines: 123, should_have: 777 }
-        problem = Tailor::Problem.new(:test, lineno, column, options)
-        problem.message(:trailing_newlines).should match /123.*777/
-      end
-    end
-
-    context "type is :hard_tab" do
-      it "builds a successful message" do
-        problem = Tailor::Problem.new(:test, lineno, column)
-        problem.message(:hard_tab).should match /Hard tab found./
-      end
-    end
-
-    context "type is :line_length" do
-      it "builds a successful message" do
-        options = { actual_length: 88, should_be_at: 77 }
-        problem = Tailor::Problem.new(:test, lineno, column, options)
-        problem.message(:line_length).should match /88.*77/
-      end
+    it "sets self[:level] to the level param" do
+      Tailor::Problem.new(:test, lineno, column, "test", :d).
+        should include(level: :d)
     end
   end
 end
