efb4a704cda539c8309b87d98feb145b0ed2712b
Steve Loveless <steve.loveless@gmail.com>
2012-04-17 00:45:00 -0700

    Fixed style and added docs

--- lib/ext/string_ext.rb
+++ lib/ext/string_ext.rb
@@ -3,10 +3,14 @@ require 'term/ansicolor'
 class String
   include Term::ANSIColor
 
+  # Borrowed from ActiveSupport, this converts camel-case Strings to
+  # snake-case.
+  #
+  # @return [String]
   def underscore
     self.gsub(/::/, '/').
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
+      gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2').
+      gsub(/([a-z\d])([A-Z])/, '\1_\2').
       tr("-", "_").
       downcase
   end
