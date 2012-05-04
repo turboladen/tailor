99b3a1ecbc52860afcd06ed7a49c6d1621bd6dc8
Steve Loveless <steve.loveless@gmail.com>
2012-05-03 17:27:20 -0700

    More config file tests

--- spec/integration/configuration_spec.rb
+++ spec/integration/configuration_spec.rb
@@ -3,15 +3,20 @@ require 'tailor/configuration'
 
 describe "Config File" do
   before do
+    Tailor::Logger.stub(:log)
     FakeFS.deactivate!
   end
 
-  context ".tailor does not exist" do
-    let!(:config) do
-      config = Tailor::Configuration.new
-      config.load!
+  let(:config) do
+    config = Tailor::Configuration.new
+    config.load!
 
-      config
+    config
+  end
+
+  context ".tailor does not exist" do
+    before do
+      Tailor::Configuration.any_instance.stub(:config_file).and_return false
     end
 
     it "sets formatters to 'text'" do
@@ -27,6 +32,66 @@ describe "Config File" do
       config.file_sets[:default].file_list.all? do |path|
         path =~ /tailor\/lib/
       end.should be_true
+    end
+  end
+
+  context ".tailor defines the default file set" do
+    context "and another file set" do
+      let(:config_file) do
+        <<-CONFIG
+Tailor.config do |config|
+  config.file_set 'lib/**/*.rb'
+
+  config.file_set 'features/**/*.rb', :features do |style|
+    style.max_line_length 90, level: :warn
+  end
+end
+        CONFIG
+      end
+
+      before do
+        File.should_receive(:read).and_return config_file
+      end
+
+      it "creates the default file set" do
+        config.file_sets[:default].style.should == Tailor::Configuration::Style.new.to_hash
+        config.file_sets[:default].file_list.all? do |path|
+          path =~ /tailor\/lib/
+        end.should be_true
+      end
+
+      it "creates the :features file set" do
+        style = Tailor::Configuration::Style.new
+        style.max_line_length(90, level: :warn)
+        config.file_sets[:features].style.should == style.to_hash
+        config.file_sets[:features].file_list.all? do |path|
+          path =~ /features/
+        end.should be_true
+      end
+    end
+  end
+
+  context ".tailor defines NO default file set" do
+    let(:config_file) do
+      <<-CONFIG
+Tailor.config do |config|
+  config.file_set 'features/**/*.rb', :features do |style|
+    style.max_line_length 90, level: :warn
+  end
+end
+      CONFIG
+    end
+
+    before do
+      File.should_receive(:read).and_return config_file
+    end
+
+    it "does not create a :default file set" do
+      config.file_sets.should_not include :default
+    end
+
+    it "creates the non-default file set" do
+      config.file_sets.should include :features
     end
   end
 end
