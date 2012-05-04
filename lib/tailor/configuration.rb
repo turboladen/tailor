1d51a6480dbbdae09a2cbe74372fc68ddbd72767
Steve Loveless <steve.loveless@gmail.com>
2012-05-03 17:27:04 -0700

    Nuked commented code, refactored

--- lib/tailor/configuration.rb
+++ lib/tailor/configuration.rb
@@ -33,57 +33,85 @@ class Tailor
     # @option options [Array] formatters
     # @option options [Hash] style
     def initialize(runtime_file_list=nil, options=nil)
-      #@style = Style.new
       @formatters = ['text']
-=begin
-      @file_sets = {
-        default: {
-          file_list: file_list(DEFAULT_GLOB),
-          style: @style.to_hash
-        }
-      }
-=end
-      @file_sets = { default: FileSet.new }
+      @file_sets = {}
 
       @runtime_file_list = runtime_file_list
       log "Got runtime file list: #{@runtime_file_list}"
+
       @options = options
       log "Got options: #{@options}"
+
+      unless @options.nil?
+        @config_file = @options.config_file unless @options.config_file.empty?
+      end
     end
 
     # Call this to load settings from the config file and from CLI options.
     def load!
       # Get config file settings
-      unless @options.nil?
-        @config_file = @options.config_file unless @options.config_file.empty?
-      end
+      if config_file
+        load_from_config_file(config_file)
 
-      load_from_config_file(config_file) if config_file
-
-      if @config_file
         if @rc_file_config
           get_formatters_from_config_file
           get_file_sets_from_config_file
         end
+      else
+        log "Creating default file set..."
+        @file_sets = { default: FileSet.new }
       end
 
       get_formatters_from_cli_opts
       get_file_sets_from_cli_opts
       get_style_from_cli_opts
-=begin
+    end
 
-      if @file_sets[:default][:file_list].empty?
-        @file_sets[:default][:file_list] = file_list(DEFAULT_GLOB)
+    # Tries to open the file at the path given at +config_file+ and read in
+    # the configuration given there.
+    #
+    # @param [String] config_file Path to the config file to use.
+    def load_from_config_file(config_file)
+      user_config_file = File.expand_path(config_file)
+
+      if File.exists? user_config_file
+        log "Loading config from file: #{user_config_file}"
+
+        begin
+          @rc_file_config = instance_eval(File.read(user_config_file), user_config_file)
+          log "Got new config from file: #{user_config_file}"
+          log @rc_file_config.inspect
+        rescue LoadError => ex
+          raise Tailor::RuntimeError,
+            "Couldn't load config file: #{user_config_file}"
+        end
+      else
+        log "No config file found at #{user_config_file}."
       end
-=end
+    end
+
+    # @return [String] Name of the config file to use.
+    def config_file
+      return @config_file if @config_file
+
+      if File.exists?(DEFAULT_PROJECT_CONFIG)
+        return @config_file = DEFAULT_PROJECT_CONFIG
+      end
+
+      if File.exists?(DEFAULT_RC_FILE)
+        return @config_file = DEFAULT_RC_FILE
+      end
     end
 
     def get_file_sets_from_config_file
       unless @rc_file_config.file_sets.empty?
         @rc_file_config.file_sets.each do |label, file_set|
-          log "file set: #{file_set}"
+          log "label: #{label}"
+          log "file set file list: #{file_set[:file_list]}"
+          log "file set style: #{file_set[:style]}"
 
           if @file_sets[label]
+            log "label already exists.  Updating..."
             #@file_sets[label][:file_list].concat file_set[:file_list]
             #@file_sets[label][:file_list].uniq!
             @file_sets[label].update_file_list(file_set[:file_list])
@@ -96,6 +124,7 @@ class Tailor
               style: @style.to_hash.merge(file_set[:style])
             }
 =end
+            log "Creating new label..."
             @file_sets[label] =
               FileSet.new(file_set[:style], file_set[:file_list])
           end
@@ -139,19 +168,6 @@ class Tailor
       end
     end
 
-    # @return [String] Name of the config file to use.
-    def config_file
-      return @config_file if @config_file
-
-      if File.exists?(DEFAULT_PROJECT_CONFIG)
-        return @config_file = DEFAULT_PROJECT_CONFIG
-      end
-
-      if File.exists?(DEFAULT_RC_FILE)
-        return @config_file = DEFAULT_RC_FILE
-      end
-    end
-
     # @return [Array] The list of formatters.
     def formatters(*new_formatters)
       @formatters = new_formatters unless new_formatters.empty?
@@ -167,98 +183,12 @@ class Tailor
     def file_set(file_glob='lib/**/*.rb', label=:default)
       log "file sets before: #{@file_sets}"
       log "file set label #{label}"
-
       new_style = Style.new
 
-      if block_given?
-        yield new_style
+      yield new_style if block_given?
 
-        if @file_sets[label]
-          #@file_sets[label][:style].merge! new_style
-          @file_sets[label].update_style(new_style)
-        end
-      end
-
-      if @file_sets[label]
-=begin
-        @file_sets[label][:file_list].concat file_list(file_glob)
-        @file_sets[label][:file_list].uniq!
-=end
-      else
-=begin
-        @file_sets[label] = {
-          file_list: file_list(file_glob),
-          style: @style.to_hash.merge(new_style)
-        }
-=end
-        @file_sets[label] = FileSet.new(new_style)
-      end
-
+      @file_sets[label] = FileSet.new(new_style, file_glob)
       log "file sets after: #{@file_sets}"
-    end
-
-    # Tries to open the file at the path given at +config_file+ and read in
-    # the configuration given there.
-    #
-    # @param [String] config_file Path to the config file to use.
-    def load_from_config_file(config_file)
-      user_config_file = File.expand_path(config_file)
-
-      if File.exists? user_config_file
-        log "Loading config from file: #{user_config_file}"
-
-        begin
-          config = instance_eval File.read(user_config_file)
-        rescue LoadError => ex
-          raise Tailor::RuntimeError,
-            "Couldn't load config file: #{user_config_file}"
-        end
-      else
-        log "No config file found at #{user_config_file}."
-      end
-
-      if config
-        log "Got new config from file: #{config}"
-        @rc_file_config = config
-      end
-    end
-
-
-    # The list of the files in the project to check.
-    #
-    # @param [String] glob Path to the file, directory or glob to check.
-    # @return [Array] The list of files to check.
-    def file_list(glob)
-=begin
-      files_in_project = if glob.is_a? Array
-        log "Configured glob is an Array: #{glob}"
-
-        glob.map do |e|
-          if File.directory?(e)
-            all_files_in_dir(e)
-          else
-            e
-          end
-        end.flatten.uniq
-      elsif File.directory? glob
-        log "Configured glob is an directory: #{glob}"
-        all_files_in_dir(glob)
-      else
-        log "Configured glob is a glob/single-file: #{glob}"
-        Dir.glob glob
-      end
-
-      list_with_absolute_paths = []
-
-      files_in_project.each do |file|
-        list_with_absolute_paths << File.expand_path(file)
-      end
-
-      log "All files: #{list_with_absolute_paths}"
-
-      list_with_absolute_paths.sort
-=end
-
     end
 
     # Displays the current configuration as a text table.
