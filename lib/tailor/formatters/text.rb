1f190f20b7cddbc88bc6c4ed010a4b3556a0c95d
Steve Loveless <steve.loveless@gmail.com>
2012-05-06 20:52:39 -0700

    Summary now alters background color lines.  Closes gh-105.
    Refactored summary into methods.  Added color to level totals based on
    the level.  Bolded Total line.  Made lines end with #.

--- lib/tailor/formatters/text.rb
+++ lib/tailor/formatters/text.rb
@@ -13,9 +13,9 @@ class Tailor
         warn: 'yellow'
       }
 
-      # @return [String] A line of "#-----", with length determined by +length+.
-      def line(length=79)
-        "##{'-' * length}\n"
+      # @return [String] A line of "#----#-", with length determined by +length+.
+      def line(length=78)
+        "##{'-' * length}#\n"
       end
 
       # @return [String] The portion of the header that displays the file info.
@@ -83,10 +83,8 @@ class Tailor
         message << file_set_header(file_set_label)
         message << problems_header(problem_list)
 
-        message << <<-MSG
-#
-#-------------------------------------------------------------------------------
-        MSG
+        message << "#\n"
+        message << line
 
         puts message
       end
@@ -98,53 +96,84 @@ class Tailor
       # @param [Hash] problems Values are filenames; keys are problems for each
       #   of those files.
       def summary_report(problems)
-        if problems.empty?
-          puts "Your files are in style."
-        else
-          summary_table = line
-          summary_table << "# " + 'Tailor Summary'.rjust(40) + "|\n".rjust(39)
-          summary_table << line
-          summary_table << '#   ' + 'File'.ljust(58) + '| Total Problems'.rjust(1) + " |\n".rjust(2)
-          summary_table << line
+        summary_table = summary_header
+        i = 0
 
-          i = 0
+        problems.each do |file, problem_list|
+          report_line = summary_file_line(file, problem_list)
 
-          problems.each do |file, problem_list|
-            file = Pathname(file)
-            relative_path = file.relative_path_from(@pwd)
-            problem_count = problem_list.size
-
-            report_line = "#{file_path(relative_path)} | " + problem_count.to_s.rjust(5) + " "
-
-            if i % 2 == 0
-              report_line = black { on_intense_black { report_line } }
-            end
-
-            summary_table << "# " << report_line << "|\n"
-            i += 1
+          report_line = if i % 2 == 1
+            on_intense_black { report_line }
+          else
+            bold { report_line }
           end
 
-          summary_table << line
-
-          problem_levels(problems).inject(summary_table) do |result, level|
-            result << "# #{level.capitalize}" + problems_at_level(problems, level).size.to_s.rjust(6) + "\n"
-          end
-
-          summary_table << line
-          summary_table << "#  TOTAL         #{problems.values.flatten.size}"
-          summary_table << line
-
-          puts summary_table
+          summary_table << "# " << report_line << reset << "|\n"
+          i += 1
         end
+
+        summary_table << line
+        summary_table << summary_level_totals(problems)
+        summary_table << "#   " << bold{ summary_first_col('TOTAL', 67) }
+        summary_table << "|"
+        summary_table << bold { total_problems(problems).to_s.rjust(6) }
+        summary_table << " |\n"
+        summary_table << line
+
+        puts summary_table
       end
 
-      def file_path(path)
-        fp = path.to_s.ljust(MAX_STRING_SIZE)
-        offset = fp.size - MAX_STRING_SIZE
+      def summary_header
+        summary_table = line
+        summary_table << "# "
+        summary_table << bold { 'Tailor Summary'.rjust(40)  }
+        summary_table << "|\n".rjust(39)
+        summary_table << line
+        summary_table << '#   ' << summary_first_col('File', 67) + '| '
+        summary_table << 'Probs'.rjust(1)
+        summary_table << " |\n".rjust(2)
+        summary_table << line
+
+        summary_table
+      end
+
+      def summary_file_line(file, problem_list)
+        file = Pathname(file)
+        relative_path = file.relative_path_from(@pwd)
+        problem_count = problem_list.size
+
+        "#{summary_first_col(relative_path)} | " + problem_count.to_s.rjust(5) + " "
+      end
+
+      def summary_first_col(path, string_size=MAX_STRING_SIZE)
+        fp = path.to_s.ljust(string_size)
+        offset = fp.size - string_size
         end_of_string = fp[offset..-1]
         end_of_string.sub!(/^.{3}/, '...') unless offset.zero?
 
         end_of_string
+      end
+
+      def summary_level_totals(problems)
+        return "" if total_problems(problems).zero?
+
+        output = problem_levels(problems).inject("") do |result, level|
+          color = PROBLEM_LEVEL_COLORS[level] || 'white'
+
+          result << "#   "
+          result << instance_eval("#{color} { summary_first_col(level.capitalize, 67) }")
+          result << "|"
+          result << instance_eval("#{color} { problems_at_level(problems, level).size.to_s.rjust(6) }")
+          result << " |\n"
+        end
+
+        output << line
+
+        output
+      end
+
+      def total_problems(problems)
+        problems.values.flatten.size
       end
     end
   end
