fe621a84205522e7e9c080151e5f0526f012c83a
Steve Loveless <steve.loveless@gmail.com>
2013-03-04 18:04:07 -0800

    Updated YAML formatter to output all problems.
    Previously, it was only outputting either the first or the last problem
    for a file, but not all problems for a file.  Relates to gh-131.

--- lib/tailor/formatters/yaml.rb
+++ lib/tailor/formatters/yaml.rb
@@ -4,7 +4,7 @@ require_relative '../formatter'
 
 class Tailor
   module Formatters
-    class Yaml < Tailor::Formatter
+    class YAML < Tailor::Formatter
       attr_reader :accepts_output_file
 
       def initialize
@@ -14,27 +14,35 @@ class Tailor
 
       # Prints the report on all of the files that just got checked.
       #
-      # @param [Hash] problems Values are filenames; keys are problems for each
+      # @param [Hash] report Values are filenames; keys are problems for each
       #   of those files.
-      def summary_report(problems)
-        build_hash(problems).to_yaml
+      def summary_report(report)
+        build_hash(report).to_yaml
       end
 
       private
-      def build_hash(problems)
-        detected = problems.select {|k,v| !v.empty?}
-        return {} if detected.empty?
 
-        detected.inject({}) do |result, hash|
-          filename = hash[0]
-          probs = hash[1].first
-          result[filename] = {
-            type: probs[:type],
-            line: probs[:line],
-            column: probs[:column],
-            message: probs[:message],
-            level: probs[:level]
-          }
+      # @param [Hash] report The list of problems found by Tailor::CLI.
+      # @return [Hash] The Hash of problems to be converted to YAML.
+      def build_hash(report)
+        report.reject! { |_, v| v.empty? }
+
+        report.inject({}) do |result, problem_set|
+          file_name = problem_set.first
+          problems = problem_set.last
+
+          problems.each do |problem|
+            result[file_name] ||= []
+
+            result[file_name] << {
+              type: problem[:type],
+              line: problem[:line],
+              column: problem[:column],
+              message: problem[:message],
+              level: problem[:level]
+            }
+          end
+
           result
         end
       end
--- spec/unit/tailor/formatters/yaml_spec.rb
+++ spec/unit/tailor/formatters/yaml_spec.rb
@@ -2,33 +2,34 @@ require_relative '../../../spec_helper'
 require 'tailor/formatters/yaml'
 require 'yaml'
 
-describe Tailor::Formatters::Yaml do
-  describe "#summary_report" do
+
+describe Tailor::Formatters::YAML do
+  describe '#summary_report' do
     let(:problems) do
       {
-        "/path_to/file1.rb" => [{
-          type: "type1", line: 23, column: 1,
-          message: "Some message", level: :error
+        '/path_to/file1.rb' => [{
+          type: 'type1', line: 23, column: 1,
+          message: 'Some message', level: :error
         }],
-        "/path_to/file2.rb" => [{
-          type: "type2", line: 23, column: 1,
-          message: "Some message", level: :error
+        '/path_to/file2.rb' => [{
+          type: 'type2', line: 23, column: 1,
+          message: 'Some message', level: :error
         }],
-        "/path_to/file3.rb" => [{
-          type: "type3", line: 23, column: 1,
-          message: "Some message", level: :error
+        '/path_to/file3.rb' => [{
+          type: 'type3', line: 23, column: 1,
+          message: 'Some message', level: :error
         }],
-        "path_to/file4.rb" => []
+        'path_to/file4.rb' => []
       }
     end
 
-    it "should return problems as yaml" do
+    it 'should return problems as yaml' do
       result = subject.summary_report(problems)
       hash = YAML.load(result)
       hash.keys.size.should == 3
-      hash.keys.first.should == "/path_to/file1.rb"
+      hash.keys.first.should == '/path_to/file1.rb'
       hash.has_key?('/path_to/file4.rb').should be_false
-      hash['/path_to/file2.rb'][:type].should eq "type2"
+      hash['/path_to/file2.rb'].first[:type].should eq 'type2'
     end
   end
 end
