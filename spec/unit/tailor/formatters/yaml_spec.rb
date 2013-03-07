d985065b90769e107758bdcf85b024d8a6a511d5
Steve Loveless <steve.loveless@gmail.com>
2013-03-06 17:32:07 -0800

    Added more cases for YAML output

--- spec/unit/tailor/formatters/yaml_spec.rb
+++ spec/unit/tailor/formatters/yaml_spec.rb
@@ -5,31 +5,71 @@ require 'yaml'
 
 describe Tailor::Formatters::Yaml do
   describe '#summary_report' do
-    let(:problems) do
-      {
-        '/path_to/file1.rb' => [{
-          type: 'type1', line: 23, column: 1,
-          message: 'Some message', level: :error
-        }],
-        '/path_to/file2.rb' => [{
-          type: 'type2', line: 23, column: 1,
-          message: 'Some message', level: :error
-        }],
-        '/path_to/file3.rb' => [{
-          type: 'type3', line: 23, column: 1,
-          message: 'Some message', level: :error
-        }],
-        'path_to/file4.rb' => []
-      }
+    context 'no files have problems' do
+      let(:problems) do
+        {
+          '/path_to/file1.rb' => [],
+          '/path_to/file2.rb' => []
+        }
+      end
+
+      it 'returns YAML with no body' do
+        result = subject.summary_report(problems)
+        hash = YAML.load(result)
+        hash.should == {}
+      end
     end
 
-    it 'should return problems as yaml' do
-      result = subject.summary_report(problems)
-      hash = YAML.load(result)
-      hash.keys.size.should == 3
-      hash.keys.first.should == '/path_to/file1.rb'
-      hash.has_key?('/path_to/file4.rb').should be_false
-      hash['/path_to/file2.rb'].first[:type].should eq 'type2'
+    context 'one file has one problem' do
+      let(:problems) do
+        {
+          '/path_to/file1.rb' => [{
+            type: 'type1', line: 23, column: 1,
+            message: 'Some message', level: :error
+          }],
+          '/path_to/file2.rb' => []
+        }
+      end
+
+      it 'returns YAML that contains the problem file and its problem' do
+        result = subject.summary_report(problems)
+        hash = YAML.load(result)
+        hash.keys.size.should == 1
+        hash.keys.first.should == '/path_to/file1.rb'
+        hash.should_not include '/path_to/file2.rb'
+        hash['/path_to/file1.rb'].first[:type].should eq 'type1'
+      end
+    end
+
+    context 'one file has one problem, another has two problems' do
+      let(:problems) do
+        {
+          '/path_to/file1.rb' => [{
+            type: 'type1', line: 23, column: 1,
+            message: 'Some message', level: :error
+          }],
+          '/path_to/file2.rb' => [],
+          '/path_to/file3.rb' => [{
+            type: 'type2', line: 45, column: 1,
+            message: 'file 3', level: :off
+          }, {
+            type: 'type3', line: 45, column: 2,
+            message: 'file 3', level: :off
+          }]
+        }
+      end
+
+      it 'returns YAML that contains the problem files and their problems' do
+        result = subject.summary_report(problems)
+        hash = YAML.load(result)
+        hash.keys.size.should == 2
+        hash.keys.first.should == '/path_to/file1.rb'
+        hash.keys.last.should == '/path_to/file3.rb'
+        hash.should_not include '/path_to/file2.rb'
+        hash['/path_to/file1.rb'].first[:type].should eq 'type1'
+        hash['/path_to/file3.rb'].first[:type].should eq 'type2'
+        hash['/path_to/file3.rb'].last[:type].should eq 'type3'
+      end
     end
   end
 end
