0bbf4f006e6a876dbc6de890b6bea3e1eb30ead9
Steve Loveless <steve.loveless@gmail.com>
2013-05-23 16:04:07 -0700

    Started specs for long method in method fix
    Relates to gh-112.

---
+++ spec/functional/horizontal_spacing/long_methods_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'tailor/critic'
+require 'tailor/configuration/style'
+
+
+LONG_METHOD_IN_CLASS = {}
+LONG_METHOD_IN_CLASS['ok_with_equals'] = <<-METH
+class Test
+  def method1
+    [1, 2, 3, 4].each do |uuid|
+      next if (@profiles[uuid].to_s.start_with? "SM" || @profiles[uuid] ==
+        :SystemLogger)
+    end
+  end
+
+  def method2
+    puts 'Do not ever get here.'
+  end
+end
+METH
+
+describe 'Long method detection' do
+  before do
+    Tailor::Logger.stub(:log)
+    FakeFS.activate!
+    File.open('long_method.rb', 'w') { |f| f.write contents }
+    subject.check_file(file_name, style.to_hash)
+  end
+
+  subject do
+    Tailor::Critic.new
+  end
+
+  let(:contents) { LONG_METHOD_IN_CLASS[file_name] }
+
+  let(:style) do
+    style = Tailor::Configuration::Style.new
+    style.trailing_newlines 0, level: :off
+    style.indentation_spaces 2, level: :off
+    style.allow_invalid_ruby true, level: :off
+    style.max_code_lines_in_method 3
+
+    style
+  end
+
+  context 'methods are within limits' do
+    context 'method ends with line that ends with ==' do
+      let(:file_name) { 'ok_with_equals' }
+      specify {
+        p subject.problems
+        subject.problems[file_name].size.should be 1 }
+    end
+  end
+end
