e97fad3be470dad5f3a05f44c910f1c7a73ae792
Steve Loveless <steve.loveless@gmail.com>
2014-01-23 09:03:06 -0800

    Randomize tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,12 @@ require 'simplecov'
 
 SimpleCov.start
 
-RSpec.configure do |conf|
-  conf.include FakeFS::SpecHelpers
+RSpec.configure do |config|
+  config.include FakeFS::SpecHelpers
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
 end
